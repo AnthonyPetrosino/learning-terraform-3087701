52bde520cfa357ee798463a14730325d7bd1fc6a
Anthony Petrosino <apetrosino789@gmail.com>
2025-02-24 21:55:58 +0000

    Module Security Group

--- main.tf
+++ main.tf
@@ -29,6 +29,19 @@ resource "aws_instance" "app" {   # resource definition: what we want, the recou
   }
 }
 
+module "app_sq" {       # taken from https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+  name = "app_new"
+
+  vpc_security_group_ids = [module.app_sg.security_group_id] 
+  
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = [all-all]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group" "app" {
   name = "app"
   description = "Allow http and https in. Allow everything out."
